f489e0d29b1368559d6395bffb5903e71eac4bfa
jakelulla <jakelulla@gmail.com>
2024-03-07 08:03:54 -0500

    new commit

---
+++ script/scripts.js
@@ -0,0 +1,14 @@
+let pageCount = 3;
+let currentPage = 0; 
+let pages; 
+
+function initializePages(){
+    pages = document.getElementsByClassName('page');
+} 
+function nextPage(){
+
+}
+
+function prevPage(){
+
+}
