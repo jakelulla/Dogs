a2c0f1301e0ac7ae55f80983ebdb15fc89313fca
jakelulla <jakelulla@gmail.com>
2024-03-11 11:08:25 -0400

    changes commit

--- main/script/script.js
+++ main/script/script.js
@@ -1,28 +1,37 @@
 let pageCount = 3;
-let currentPage = 0; 
-let pages; 
+let currentPage = 0;
+let pages;
 
 initializePages();
-function initializePages(){
+
+function initializePages() {
     pages = document.getElementsByClassName('page');
-} 
-function nextPage(){
+}
+
+function nextPage() {
     if(currentPage + 1 >= pageCount)
         return;
+
     pages[currentPage].classList.toggle('hidden');
+    
     currentPage++;
+    
     pages[currentPage].classList.toggle('hidden');
 }
 
-function prevPage(){
-    if(currentPage == 0)
+function prevPage() {
+    if(currentPage - 1 < 0)
         return;
+
     pages[currentPage].classList.toggle('hidden');
+    
     currentPage--;
+    
     pages[currentPage].classList.toggle('hidden');
 }
 
-function toggleNav(){
+function toggleNav() {
     let navbar = document.getElementById('sidebar-nav');
-    navbar.classList.toggle('hidden');
+    navbar.classList.toggle('sidebar-shown');
 }
+
